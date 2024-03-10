cd77e7e777adade844062a21e0aabf35a6887c73
Dalibor Janeček <dalibor.janecek@gmail.com>
2024-03-10 12:37:36 +0100

    Initial Commit

---
+++ GustavWeights/GustavWeightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GustavWeightsApp.swift
+//  GustavWeights
+//
+//  Created by Dalibor Janeček on 10.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct GustavWeightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
